264de477bc0fc8262cf5622824fa267907db8472
Santosh Mahanta <62832053+Hacking73@users.noreply.github.com>
2026-08-03 14:07:08 +0530

    script.js

--- script.js
+++ script.js
@@ -12,3 +12,43 @@ document.getElementById("openBtn").addEventListener("click", () => {
     alert("Working");
     showPage(1);
 });
+const scratch = document.getElementById("scratchCanvas");
+const sctx = scratch.getContext("2d");
+
+function resetScratch() {
+
+    sctx.globalCompositeOperation = "source-over";
+
+    sctx.fillStyle = "#b784ff";
+    sctx.fillRect(0,0,scratch.width,scratch.height);
+
+    sctx.fillStyle = "#fff";
+    sctx.font = "24px Poppins";
+    sctx.textAlign = "center";
+    sctx.fillText("Scratch Here ❤️",160,75);
+
+}
+
+resetScratch();
+
+let drawing = false;
+
+scratch.addEventListener("mousedown",()=>drawing=true);
+scratch.addEventListener("mouseup",()=>drawing=false);
+scratch.addEventListener("mouseleave",()=>drawing=false);
+
+scratch.addEventListener("mousemove",(e)=>{
+
+    if(!drawing) return;
+
+    const rect = scratch.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    sctx.globalCompositeOperation = "destination-out";
+    sctx.beginPath();
+    sctx.arc(x,y,20,0,Math.PI*2);
+    sctx.fill();
+
+});
